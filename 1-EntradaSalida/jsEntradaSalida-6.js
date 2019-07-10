a5536285c15200c7c9e1afdf8a590c2a59e02cbd
juancc1001 <52534704+juancc1001@users.noreply.github.com>
2019-07-10 18:52:05 -0300

    10/07

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -2,7 +2,15 @@
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
-{
+{var numero1;
+    numero1 = document.getElementById("numeroUno").value;
+var numero2;
+    numero2 = document.getElementById("numeroDos").value;
 
+var suma;
+suma = (parseInt(numero1)+parseInt(numero2));
+
+alert("la suma es " + suma);
 }
+
 
