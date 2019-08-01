d0799798e31ed3621ab73a4c97be8252925c60f2
juancc1001 <52534704+juancc1001@users.noreply.github.com>
2019-08-01 12:13:22 -0300

    01/08

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -2,32 +2,33 @@
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2
-bolsas de cemento y 3 de cal, debemos mostrar cuantas 
+bolsas de cemento y 3 de cal, debemos mostrar cuantas
 bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
 
-function Rectangulo () 
-{ 
-    var largo = document.getElementById("Largo").value;
-    var ancho = document.getElementById("Ancho").value;
-    parseInt(largo);
-    parseInt(ancho);
-    var alambre1=(largo*ancho*3);
-    alert("la cantidad de alambre debe ser de " + alambre1);
+function Rectangulo ()
+{
+  var largo=parseInt(document.getElementById("Largo").value);
+  var ancho=parseInt(document.getElementById("Ancho").value);
+  var alambre1=(largo*ancho*3);
+  alert("la cantidad de alambre debe ser de " + alambre1 + " m");
 }
-function Circulo () 
-{   var radio=  document.getElementById("Radio").value;
-    parseInt(radio);
-    var alambre2=(radio*2*3.14);
-    alert("la cantidad de alambre debe ser" + alambre2);
-    	
+
+function Circulo ()
+{
+  var radio=parseInt(document.getElementById("Radio").value);
+  var alambre2=(radio*2*3.14);
+  alert("la cantidad de alambre debe ser " + alambre2 +" m");
+
 }
-function Materiales () 
-{   
-    var perimetro=(largo*ancho);
-    parseInt(perimetro);
-    var cemento=(perimetro*2);
-    var cal=(perimetro*3);
-    alert("cant de bolsas de cemento: "+ cemento + ", y cant de bolsas de cal: " + cal);
+
+function Materiales ()
+{
+  var largo=parseInt(document.getElementById("Largo").value);
+  var ancho=parseInt(document.getElementById("Ancho").value);
+  var perimetro=parseInt(largo*ancho);
+  var cemento=(perimetro*2);
+  var cal=(perimetro*3);
+  alert("cant de bolsas de cemento: "+ cemento + ", y cant de bolsas de cal: " + cal);
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,23 +3,32 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-var precio1=document.getElementById("PrecioUno").value;
-var precio2=document.getElementById("PrecioDos").value;
-var precio3=document.getElementById("PrecioTres").value;
-parseInt(precio1);
-parseInt(precio2);
-parseInt(precio3);
-var suma=(precio1+precio2+precio3);
 
-function Sumar () 
-{   
-    alert("la suma es " + suma);
+
+function Sumar ()
+{
+  var precio1=parseInt(document.getElementById("PrecioUno").value);
+  var precio2=parseInt(document.getElementById("PrecioDos").value);
+  var precio3=parseInt(document.getElementById("PrecioTres").value);
+  var suma=(precio1+precio2+precio3);
+  alert("la suma es " + suma);
 }
-function Promedio () 
-{   var promedio = (suma/3);
+
+function Promedio ()
+{
+  var precio1=parseInt(document.getElementById("PrecioUno").value);
+  var precio2=parseInt(document.getElementById("PrecioDos").value);
+  var precio3=parseInt(document.getElementById("PrecioTres").value);
+  var suma=(precio1+precio2+precio3);
+  var promedio = (suma/3);
 	alert("el promedio es " + promedio);
 }
-function PrecioFinal () 
-{   var PrecioFinal=(suma+suma*0.21);
-    alert("el precio final es: " + PrecioFinal);	
+function PrecioFinal ()
+{
+  var precio1=parseInt(document.getElementById("PrecioUno").value);
+  var precio2=parseInt(document.getElementById("PrecioDos").value);
+  var precio3=parseInt(document.getElementById("PrecioTres").value);
+  var suma=(precio1+precio2+precio3);
+  var PrecioFinal=(suma+suma*0.21);
+  alert("el precio final es: " + PrecioFinal);
 }
